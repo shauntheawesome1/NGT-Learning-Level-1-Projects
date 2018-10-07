c75fd6177cd242394b6a84d0a4eab487f17b7d1f
shauntheawesome1 <Shaun.munshi@gmail.com>
2018-10-07 12:35:49 -0700

    Update and rename printName.java to addNumbers.java

--- addNumbers.java
+++ addNumbers.java
@@ -1,21 +1,4 @@
-/***** PRINTNAME.JAVA*******/
-package com.ngtlearning.example;
 
-import java.util.Scanner;
-
-public class PrintName {
-
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter Your name: ");
-		String  name = s.next();
-		System.out.println("Hello "+name+ ". You are Awesome.");
-		s.close();
-		
-
-	}
-
-}
 /***** ADDNUMBERS.JAVA*******/
 package com.ngtlearning.example;
 
