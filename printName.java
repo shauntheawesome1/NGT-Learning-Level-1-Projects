f8ee372c1167e101db802e44f84b45826832c0b7
shauntheawesome1 <Shaun.munshi@gmail.com>
2018-10-07 12:27:33 -0700

    Create printName.java

---
+++ printName.java
@@ -0,0 +1,44 @@
+/***** PRINTNAME.JAVA*******/
+package com.ngtlearning.example;
+
+import java.util.Scanner;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Your name: ");
+		String  name = s.next();
+		System.out.println("Hello "+name+ ". You are Awesome.");
+		s.close();
+		
+
+	}
+
+}
+/***** ADDNUMBERS.JAVA*******/
+package com.ngtlearning.example;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		
+		int number1 = 10;
+		int number2 = 20;
+		int number3 = 30;
+		int total1;
+		int total2;
+		int total3;
+		
+		total1 = number1 + number2;
+		System.out.println("The Addition of "+number1+" and "+number2+" is = "+total1);
+		
+		total2 = number2 + number3;
+		System.out.println("The Addition of "+number2+" and "+number3+" is = "+total2);
+		
+		total3 = number1 + number3;
+		System.out.println("The Addition of "+number1+" and "+number3+" is = "+total3);
+
+	}
+
+}
